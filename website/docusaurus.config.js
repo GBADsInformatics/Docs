5ec20e5a2e2a9ed29ec4fe91ca498934dd191359
MatthewSzurkowski <mszurkow@gmail.com>
2022-11-23 16:11:16 -0500

    Small fix.

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,7 +8,7 @@ const config = {
   title: 'GBADs Informatics',
   tagline: 'GBADs Informatics',
   url: 'https://gbadsinformatics.github.io/',
-  baseUrl: '/',
+  baseUrl: '/docs/',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
