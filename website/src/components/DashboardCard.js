8f646db97d76d5267556b998ba330884bbbaad57
MatthewSzurkowski <mszurkow@gmail.com>
2023-06-28 18:27:20 -0400

    Update Dashboards page.

--- website/src/components/DashboardCard.js
+++ website/src/components/DashboardCard.js
@@ -3,64 +3,26 @@ import "./styles/DashboardCard.css";
 import clsx from 'clsx';
 
 function DashboardCard(props){
-    // console.log(props.piclink);
-    // console.log(props.relatedLinks);
-
-    if (props.listitems.length==0){
-        return (
-            <div className={clsx('col col--4 padding-vert--md')}>
-                <div className="text--center padding-horiz--md homepage-card">
-                    {/* <div className="dash-pic-container">
-                        <img src={props.piclink} width="80%" height="100%"/>
-                    </div> */}
-                    <div className="dash-text-container" style={{width: "75%"}}>
-                        <h1 className="dash-title">{props.title}</h1>
-                        <p className="dash-dolu">{props.dolu}</p>
-                        <p className="dash-desc">{props.desc}</p>
-                        {typeof props.relatedLinks !== 'undefined' &&
-              <ul style={{ listStyleType: "none" }}>
-                {props.relatedLinks.map((link, idx) => (
-                  <li key={idx}>
-                    <a href={Object.values(link)[0]}>{Object.keys(link)[0]}</a>
-                  </li>
-                ))}
-              </ul>
-            }
-                    </div>
-                </div>
-            </div>
-        );
-    }
-    else{
-    return(
+  return (
         <div className={clsx('col col--4 padding-vert--md')}>
             <div className="text--center padding-horiz--md homepage-card">
-                {/* <div className="dash-pic-container">
-                    <img src={props.piclink} width="80%" height="100%"/>
-                </div> */}
                 <div className="dash-text-container" style={{width: "75%"}}>
                     <h1 className="dash-title">{props.title}</h1>
-                    <p className="dash-dolu">{props.dolu}</p>
-                    <p className="dash-desc">{props.desc}</p>
-                    <ul className="mainDashBullets" style={{listStyleType: "none", marginLeft: "-5%"}}>
-                    {props.listitems.map((item, idx) => (
-                        <li style={{fontWeight: "bold"}} key={idx}>{item}</li>
-                    ))}
-                    </ul>
+                    <div className="align-ul-element">
                     {typeof props.relatedLinks !== 'undefined' &&
-              <ul className="mainDashBullets" style={{ listStyleType: "none", marginLeft: "-5%"}}>
-                {props.relatedLinks.map((link, idx) => (
-                  <li key={idx}>
-                    <a href={Object.values(link)[0]}>{Object.keys(link)[0]}</a>
-                  </li>
-                ))}
-              </ul>
-            }
+                      <ul style={{ listStyleType: "none", textAlign: 'center' }}>
+                        {props.relatedLinks.map((link, idx) => (
+                          <li key={idx}>
+                            <a href={Object.values(link)[0]}>{Object.keys(link)[0]}</a>
+                          </li>
+                        ))}
+                      </ul>
+                    }
+                    </div>
                 </div>
             </div>
         </div>
     );
-                    }
 }
 
 export default DashboardCard;
--- website/src/pages/Dashboards.js
+++ website/src/pages/Dashboards.js
@@ -14,159 +14,79 @@ function Dashboards() {
         <Translate>Population</Translate>
         </>
         ),
-        dolu: (
-            <Translate>
-              Date of Last Update: 2023-06-02
-            </Translate>
-          ),
-      desc: (
-        <Translate>
-          The Animal Population dashboard displays animal populations by species per year per country (or region or for the world). Current data sources include:
-        </Translate>
-      ),
-      listitems: ["FAOSTAT", "WOAH (formerly OIE) (2021-08-18)", "EUROSTAT"],
       relatedLinks: [
-          {"Population Dashboard Version 2": "https://www.gbadske.org/dashboards/population-v2/"},
-          {"Population Dashboard Version 1": "https://gbadske.org/dashboards/population/"},
+          {"National Population": "https://gbadske.org/dashboards/population-v2/"},
+          {"Ethiopia Sub-National Population": "https://gbadske.org/dashboards/ethiopia-population/"},
+    ] 
+    },
+    {
+      title: (
+        <>
+        <Translate>Biomass</Translate>
+        </>
+        ),
+      relatedLinks: [
+          {"Biomass": "https://gbadske.org/dashboards/biomass-v2/"},
     ]
      
     },
     {
       title: (
         <>
-        <Translate>Animal Health Loss Envelope</Translate>
+        <Translate>Analytics</Translate>
         </>
         ),
-        dolu: (
-            <Translate>
-              Date of Last Update: 2023-05-02
-            </Translate>
-          ),
-      desc: (
-        <Translate>
-          The (First Analytics and University of Liverpool) Animal Health Loss Envelope dashboard provides analytics and visualizations for the Ethiopia Case Study, Antimicrobrial Resistance, and the Animal Health Loss Envelope. 
-        </Translate>
-      ),
-      listitems:[],
-
-      relatedLinks: [{
-        "Animal Health Loss Envelope Dashboard": "https://gbadske.org/dashboards/ahle/"
-      }
+      relatedLinks: [
+          {"Total Economic Value": "https://gbadske.org/dashboards/tev/"},
+          {"Animal Health Loss Envelope": "https://gbadske.org/dashboards/ahle/"},
     ]
+     
     },
     {
-        title: (
-          <>
-          <Translate>Biomass</Translate>
-          </>
-          ),
-          dolu: (
-              <Translate>
-                Date of Last Update: 2023-05-02
-              </Translate>
-            ),
-        desc: (
-          <Translate>
-            The Biomass dashboard displays the results of various different ways to calculate biomass. Current calculations include: 
-          </Translate>
+      title: (
+        <>
+        <Translate>Data Portal</Translate>
+        </>
         ),
-        listitems: ["GBADs PPS calculation method", "WOAH AMR calculation method"],
-
-        relatedLinks: [
-          {"Biomass Dashboard Version 1": "https://gbadske.org/dashboards/biomass/"},
-          {"Biomass Dashboard Version 2": "https://gbadske.org/dashboards/biomass-v2/"}
-      ]
-      },
-      {
-        title: (
-          <>
-          <Translate>Laying Hens Dashboard</Translate>
-          </>
-          ),
-          dolu: (
-              <Translate>
-                Date of Last Update: 2023-03-14
-              </Translate>
-            ),
-        desc: (
-          <Translate>
-            The Laying Hens dashboard displays data on the welfare of laying hens from the European Commission.
-          </Translate>
+      relatedLinks: [
+          {"GBADs API Explorer": "https://gbadske.org/dashboards/apiui/"},
+    ]
+     
+    },
+    {
+      title: (
+        <>
+        <Translate>Other</Translate>
+        </>
         ),
-        listitems:[],
-  
-        relatedLinks: [{"Laying Hens Dashboard": "https://gbadske.org/dashboards/layinghens/"}]
-      },
-      {
-        title: (
-          <>
-          <Translate>Ethiopia Sub-National Population</Translate>
-          </>
-          ),
-          dolu: (
-              <Translate>
-                Date of Last Update: 2023-05-02
-              </Translate>
-            ),
-        desc: (
-          <Translate>
-           This dashboard allows users to plot, map, and download data from the Ethiopia Central Statistics Agency
-          </Translate>
+      relatedLinks: [
+          {"Laying Hens": "https://gbadske.org/dashboards/layinghens/"},
+    ]
+     
+    },
+    {
+      title: (
+        <>
+        <Translate>In Development</Translate>
+        </>
         ),
-        listitems:[],
-
-        relatedLinks: [{"Ethiopia Sub-National Population": "https://gbadske.org/dashboards/ethiopia-population/"}]
-      },
-        {
-          title: (
-            <>
-            <Translate>GBADs API User Interface</Translate>
-            </>
-            ),
-            dolu: (
-                <Translate>
-                  Date of Last Update: 2023-05-02
-                </Translate>
-              ),
-          desc: (
-            <Translate>
-              This dashboard allows users to visualize and download data available in the GBADs API
-            </Translate>
-          ),
-          listitems:[],
-  
-          relatedLinks: [{"GBADs API User Interface": "https://www.gbadske.org/dashboards/apiui/"}]
-        },
-        {
-          title: (
-            <>
-            <Translate>In Development</Translate>
-            </>
-            ),
-            dolu: (
-                <Translate>
-                  Date of Last Update: 2023-05-02
-                </Translate>
-              ),
-          desc: (
-            <Translate>
-              Check out our dashboards that are currently under development
-            </Translate>
-          ),
-          listitems:[],
-  
-          relatedLinks: [{"Data Visualizer": "https://www.gbadske.org/dashboards/visualizer/"},
-                         {"Ethiopia Data Stories": "https://www.gbadske.org/dashboards/datastories/"}]
-        },
+      relatedLinks: [
+          {"Data Visualizer": "https://gbadske.org/dashboards/visualizer/"},
+          {"Ethiopia Data Stories": "https://gbadske.org/dashboards/datastories/"},
+    ]
+     
+    },
+    
       ]
 
   return (
     <Layout>
           <div className='lower-container'>
-            <br/>
-            <center>
-                <h1><u>The Knowledge Engine Dashboards</u></h1>
-            </center>
+            <div className='dash-title-container'>
+                <h1 style={{color: 'white', display: 'inline'}}>The Knowledge</h1>
+                <h1 style={{color: '#f7931d' , display: 'inline'}}> Engine Dashboards</h1>
+                <hr style={{width: '5%', textAlign: 'center', margin: 'auto', marginTop: '1%'}}/>
+            </div>
             <div className="row homepage-highligh-row">
                 {DashInfo.map((props, idx) => (
                 <DashboardCard key={idx} {...props} />
