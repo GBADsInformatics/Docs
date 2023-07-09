20c85c0be6f5b2c50984b8bcd610cd93f19dde23
MatthewSzurkowski <mszurkow@gmail.com>
2023-07-09 17:07:06 -0400

    Old push. Updated description on dashboards page.

--- website/src/pages/Dashboards.js
+++ website/src/pages/Dashboards.js
@@ -7,6 +7,12 @@ import Layout from '@theme/Layout';
 import "./styles/Dashboards.css";
 
 function Dashboards() {
+
+  const dashboard_desc = <><Translate>Welcome to the Global Burden of Animal Diseases Dashboard Landing Page! This page provides links to our interactive dashboards that we are using to explore data and results towards our goal of calculating the economic and health disease burden of animal diseases.</Translate><br/><br/>
+  <Translate>The Population Dashboards provide access to livestock population data at national and subnational levels, while the Biomass Dashboard quantifies livestock biomass using different calculations. Analytical Dashboards show the results of our analytics and modelling efforts, such as the calculation of the Animal Health Loss Envelope and Total Economic Value. Explore the consistency of datasets with the FAOSTAT Visualizer dashboard and explore Data Stories through our Data Stories dashboards. To access and discover the datasets we are using in our calculations, use the Data Portal dashboards.</Translate><br/><br/>
+  <Translate>Collaborate, stay updated, and contribute your own data to enhance animal health worldwide. Our dashboards empower data-driven decision-making and facilitate collaboration for improved animal health and well-being worldwide.</Translate></>
+
+
   const DashInfo = [
     {
       title: (
@@ -86,6 +92,9 @@ function Dashboards() {
                 <h1 style={{color: 'white', display: 'inline'}}>The Knowledge</h1>
                 <h1 style={{color: '#f7931d' , display: 'inline'}}> Engine Dashboards</h1>
                 <hr style={{width: '5%', textAlign: 'center', margin: 'auto', marginTop: '1%'}}/>
+            </div>
+            <div style={{width:"75%", textAlign: "center", marginTop: "2.5%", backgroundColor:"#f1f1f1", padding: '2%', borderRadius: '25px'}}>
+              <p>{dashboard_desc}</p>
             </div>
             <div className="row homepage-highligh-row">
                 {DashInfo.map((props, idx) => (
